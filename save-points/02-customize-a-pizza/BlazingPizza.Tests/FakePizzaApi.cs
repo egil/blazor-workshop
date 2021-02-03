4dcb9e100f060490c5e5089b7c75c9ff1698f2a7
Egil Hansen <egil@assimilated.dk>
2021-02-03 16:19:39 +0000

    Added ConfigurePizzaDialog with tests, included in Index page with test of change to Index page

---
+++ save-points/02-customize-a-pizza/BlazingPizza.Tests/FakePizzaApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlazingPizza.Client.Services;
+
+namespace BlazingPizza
+{
+    public class FakePizzaApi : IPizzaApi
+    {
+        private readonly IReadOnlyList<PizzaSpecial> pizzas;
+
+        public FakePizzaApi(IEnumerable<PizzaSpecial> pizzas) => this.pizzas = pizzas.ToArray();
+
+        public Task<IReadOnlyList<PizzaSpecial>> GetPizzaSpecialsAsync() => Task.FromResult(pizzas);
+    }
+}
