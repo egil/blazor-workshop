345a51ab716e6885a059bd7d64216cc90776417d
Egil Hansen <egil@assimilated.dk>
2021-02-17 13:38:42 +0000

    Removed shared components that are stored in the component library, cleaned up the main layout test

--- save-points/03-show-order-status/BlazingPizza.Tests/FakeNavigationManager.cs
+++ save-points/03-show-order-status/BlazingPizza.Tests/FakeNavigationManager.cs
@@ -1,23 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Bunit.Rendering;
 using Microsoft.AspNetCore.Components;
 
 namespace BlazingPizza
 {
     public class FakeNavigationManager : NavigationManager
     {
-        public FakeNavigationManager()
+        private readonly ITestRenderer renderer;
+
+        public FakeNavigationManager(ITestRenderer renderer)
         {
             Initialize("http://localhost/", "http://localhost/");
+            this.renderer = renderer;
         }
 
         protected override void NavigateToCore(string uri, bool forceLoad)
         {
             Uri = ToAbsoluteUri(uri).ToString();
-            NotifyLocationChanged(isInterceptedLink: false);
+
+            renderer.Dispatcher.InvokeAsync(
+                () => NotifyLocationChanged(isInterceptedLink: false));
         }
     }
 }
