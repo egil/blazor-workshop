b0f7d28e8f5181b348c90614c79e6e51ba548dfe
Egil Hansen <egil@assimilated.dk>
2021-02-24 20:37:55 +0000

    Save point 4 done

---
+++ save-points/04-refactor-state-management/BlazingPizza.Tests/FakeNavigationManager.cs
@@ -0,0 +1,25 @@
+﻿using Bunit;
+using Bunit.Rendering;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazingPizza
+{
+    public class FakeNavigationManager : NavigationManager
+    {
+        private readonly TestContext context;
+
+        public FakeNavigationManager(TestContext context)
+        {
+            this.context = context;            
+            Initialize("http://localhost/", "http://localhost/");
+        }
+
+        protected override void NavigateToCore(string uri, bool forceLoad)
+        {
+            Uri = ToAbsoluteUri(uri).ToString();
+
+            context.Renderer.Dispatcher.InvokeAsync(
+                () => NotifyLocationChanged(isInterceptedLink: false));
+        }
+    }
+}
---
+++ save-points/04-refactor-state-management/BlazingPizza.Tests/LoadingForeverPizzaApi.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using BlazingPizza.Client.Services;
+
+namespace BlazingPizza
+{
+    public sealed class LoadingForeverPizzaApi : IPizzaApi, IDisposable
+    {
+        private readonly TaskCompletionSource loadTask = new TaskCompletionSource();
+
+        public void Dispose()
+        {
+            loadTask.SetResult();
+        }
+
+        public async Task<IReadOnlyList<OrderWithStatus>> GetOrdersWithStatusAsync()
+        {
+            await loadTask.Task;
+            return Array.Empty<OrderWithStatus>();
+        }
+
+        public async IAsyncEnumerable<OrderWithStatus> GetOrderUpdatesById(int orderId, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            await loadTask.Task;
+            yield break;
+        }
+
+        public async Task<IReadOnlyList<PizzaSpecial>> GetPizzaSpecialsAsync()
+        {
+            await loadTask.Task;
+            return Array.Empty<PizzaSpecial>();
+        }
+
+        public async Task<IReadOnlyList<Topping>> GetToppingsAsync()
+        {
+            await loadTask.Task;
+            return Array.Empty<Topping>();
+        }
+
+        public async Task<int> PlaceOrderAsync(Order order)
+        {
+            await loadTask.Task;
+            return 0;
+        }
+    }
+}
