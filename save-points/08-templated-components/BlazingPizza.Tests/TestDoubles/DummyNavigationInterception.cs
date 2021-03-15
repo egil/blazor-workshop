c336cc1d2d9a1debcaf7a02b56c8afbe5c5ac62e
Egil Hansen <egil@assimilated.dk>
2021-03-15 15:15:11 +0000

    Reset chapter 8 save-point to chapter 7

---
+++ save-points/08-templated-components/BlazingPizza.Tests/TestDoubles/DummyNavigationInterception.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace BlazingPizza.TestDoubles
+{
+    public class DummyNavigationInterception : INavigationInterception
+    {
+        public Task EnableNavigationInterceptionAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ save-points/08-templated-components/BlazingPizza.Tests/TestDoubles/DummyRemoteAuthenticationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+namespace BlazingPizza.TestDoubles
+{
+    public class DummyRemoteAuthenticationService<TRemoteAuthenticationState> : IRemoteAuthenticationService<TRemoteAuthenticationState>
+        where TRemoteAuthenticationState : RemoteAuthenticationState
+    {
+        public Task<RemoteAuthenticationResult<TRemoteAuthenticationState>> CompleteSignInAsync(RemoteAuthenticationContext<TRemoteAuthenticationState> context)
+        {
+            throw new NotImplementedException("CompleteSignInAsync");
+        }
+
+        public Task<RemoteAuthenticationResult<TRemoteAuthenticationState>> CompleteSignOutAsync(RemoteAuthenticationContext<TRemoteAuthenticationState> context)
+        {
+            throw new NotImplementedException("CompleteSignOutAsync");
+        }
+
+        public Task<RemoteAuthenticationResult<TRemoteAuthenticationState>> SignInAsync(RemoteAuthenticationContext<TRemoteAuthenticationState> context)
+        {
+            throw new NotImplementedException("SignInAsync");
+        }
+
+        public Task<RemoteAuthenticationResult<TRemoteAuthenticationState>> SignOutAsync(RemoteAuthenticationContext<TRemoteAuthenticationState> context)
+        {
+            throw new NotImplementedException("SignOutAsync");
+        }
+    }
+}
